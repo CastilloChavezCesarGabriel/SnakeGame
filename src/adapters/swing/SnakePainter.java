7f8569c0c0b2743c17b861cac1f88546917fe113
cesargabrielcastillochavez <cesar.castillo7529@alumnos.udg.mx>
2026-03-20 16:33:10 -0600

    New Commit

--- src/adapters/swing/SnakePainter.java
+++ src/adapters/swing/SnakePainter.java
@@ -1,13 +1,14 @@
 package adapters.swing;
 
 import java.awt.Color;
+import java.awt.Graphics2D;
 
 public final class SnakePainter extends Painter {
     private static final Color BODY = new Color(74, 222, 128);
     private static final Color EDGE = new Color(34, 150, 74);
     private static final int ARC = 6;
 
-    public SnakePainter(java.awt.Graphics2D graphics, int cellSize) {
+    public SnakePainter(Graphics2D graphics, int cellSize) {
         super(graphics, cellSize);
     }
 
